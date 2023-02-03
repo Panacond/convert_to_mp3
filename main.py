6ccd385f0bf7e4ce2c7c139eaa644f5a82961c92
akakiy <ivachkevich63@gmail.com>
2023-02-03 15:53:04 +0200

    add title

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import os
 
 if __name__ == "__main__":
-    input_file = "cartoon.mp4"
+    input_file = "input.mp4"
     list_song = [
-        ["farm","00:00:10","00:02:31"],
-        ["train","00:02:51","00:06:20"],
-        ["frend farm","00:06:39","00:08:51"],
-        ["leo friend","00:09:06","00:11:36"],
-        ["3 wave","00:14:25","00:17:20"],
-        ["fruits","00:19:52","00:21:51"]
+        ["title","00:59:11","01:01:32"]
     ]
 
     for i in list_song:
-        output_file = f"'leo the truck - {i[0]}.mp3'"
+        artist = "artist"
+        output_file = f"'{artist} - {i[0]}.mp3'"
         time_start = i[1]
         time_finish = i[2]
-        command = f"ffmpeg -i {input_file} -ss {time_start} -to {time_finish} -metadata artist='leo' {output_file}"
+        command = f"ffmpeg -i {input_file} -ss {time_start} -to {time_finish} -metadata artist='{artist}' -metadata title='{i[0]}' {output_file}"
         print(command)
         os.system(command)
